bcfa20c8a61ac1ebe134d5884396cc7df35f9291
SHKim55 <5061ksh@naver.com>
2025-04-16 12:30:19 +0900

    feat: 구글 로그인에 필요한 통신 객체 생성

---
+++ src/main/java/com/mocamp/mocamp_backend/service/login/GoogleLoginRequest.java
@@ -0,0 +1,25 @@
+package com.mocamp.mocamp_backend.service.login;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoogleLoginRequest {
+    private String code;
+
+    private String clientId;
+
+    private String clientSecret;
+
+    private String redirectUri;
+
+    private String grantType;
+}
