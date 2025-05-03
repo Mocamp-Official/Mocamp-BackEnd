0639f70faf0b5d2f208f55a2a60a178e4688605d
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-05-03 21:04:43 +0900

    feat: 카카오 로그인 응답 객체 생성

---
+++ src/main/java/com/mocamp/mocamp_backend/dto/kakao/KakaoLoginResponse.java
@@ -0,0 +1,17 @@
+package com.mocamp.mocamp_backend.dto.kakao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class KakaoLoginResponse {
+
+    private Long id;
+    private String email;
+    private String username;
+    private String accessToken;
+    private String refreshToken;
+}
