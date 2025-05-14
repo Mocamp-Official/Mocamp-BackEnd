e2717917116af130a3011c51b573ed9708cf66da
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-05-14 21:59:28 +0900

    fix: 네이버 로그인 리디렉션 구조 변경

--- src/main/java/com/mocamp/mocamp_backend/controller/LoginController.java
+++ src/main/java/com/mocamp/mocamp_backend/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.mocamp.mocamp_backend.controller;
 
 import com.mocamp.mocamp_backend.dto.commonResponse.CommonResponse;
 import com.mocamp.mocamp_backend.dto.loginResponse.LoginResponse;
-import com.mocamp.mocamp_backend.dto.naver.NaverLoginResponse;
 import com.mocamp.mocamp_backend.service.login.GoogleLoginService;
 import com.mocamp.mocamp_backend.service.login.KakaoLoginService;
 import com.mocamp.mocamp_backend.service.login.NaverLoginService;
@@ -68,20 +67,25 @@ public class LoginController {
 
     @Operation(
             summary = "네이버 로그인 페이지 로딩",
+            parameters = {@Parameter(name = "redirect_url", description = "리디렉션 될 주소")},
             responses = { @ApiResponse(responseCode = "200", description = "URL 반환 성공") }
     )
     @GetMapping("/naver/page")
-    public ResponseEntity<CommonResponse> loadNaverLoginPage() {
-        return naverLoginService.loadNaverLoginPage();
+    public ResponseEntity<CommonResponse> loadNaverLoginPage(@RequestParam(name = "redirect_url") String redirect_url) {
+        return naverLoginService.loadNaverLoginPage(redirect_url);
     }
 
     @Operation(
             summary = "네이버 로그인 리다이렉션 URI",
-            parameters = { @Parameter(name = "code", description = "로그인 후 네이버 서버에서 반환하는 코드") }
+            parameters = {
+                    @Parameter(name = "code", description = "로그인 후 네이버 서버에서 반환하는 코드"),
+                    @Parameter(name = "redirect_url", description = "네이버 인가 코드 요청 시 사용한 redirect_uri와 동일한 값")
+            },
+            responses = {@ApiResponse(responseCode = "200", description = "로그인 성공 - JWT 토큰 반환")}
     )
     @GetMapping("/naver/process")
-    public ResponseEntity<NaverLoginResponse> naverLogin(@RequestParam(name = "code") String code) {
-        NaverLoginResponse naverLoginResponse = naverLoginService.naverLogin(code);
-        return ResponseEntity.ok(naverLoginResponse);
+    public ResponseEntity<LoginResponse> naverLogin(@RequestParam(name = "code") String code, @RequestParam(name = "redirect_url") String redirect_url) {
+        LoginResponse loginResponse = naverLoginService.naverLogin(code, redirect_url);
+        return ResponseEntity.ok(loginResponse);
     }
 }
