49ad988b98c833608ddf197c12ab61bec543f29e
SHKim55 <5061ksh@naver.com>
2025-05-24 19:52:02 +0900

    refactor: 필드 자료형 변경

--- src/main/java/com/mocamp/mocamp_backend/dto/room/RoomEnterRequest.java
+++ src/main/java/com/mocamp/mocamp_backend/dto/room/RoomEnterRequest.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class RoomEnterRequest {
-    private String micTurnedOn;
-    private String camTurnedOn;
+    private Boolean micTurnedOn;
+    private Boolean camTurnedOn;
 }
