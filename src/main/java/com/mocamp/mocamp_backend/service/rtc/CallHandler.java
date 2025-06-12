1982ffc67ee828f11018410714dc07302ad66565
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-06-13 01:38:24 +0900

    feat: WebRTC 로직 구현

---
+++ src/main/java/com/mocamp/mocamp_backend/service/rtc/CallHandler.java
@@ -0,0 +1,87 @@
+package com.mocamp.mocamp_backend.service.rtc;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kurento.client.IceCandidate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CallHandler extends TextWebSocketHandler {
+
+    private static final Gson gson = new GsonBuilder().create();
+    private final RoomManager roomManager;
+    private final UserRegistry registry;
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        final JsonObject jsonMessage = gson.fromJson(message.getPayload(), JsonObject.class);
+        final UserSession user = registry.getBySession(session);
+
+        if (user != null) {
+            log.debug("기존 유저로부터의 메시지 '{}': {}", user.getName(), jsonMessage);
+        } else {
+            log.debug("새로운 유저로부터의 메시지: {}", jsonMessage);
+        }
+
+        switch (jsonMessage.get("id").getAsString()) {
+            case "joinRoom":
+                joinRoom(jsonMessage, session);
+                break;
+            case "receiveVideoFrom":
+                final String senderName = jsonMessage.get("sender").getAsString();
+                final UserSession sender = registry.getByName(senderName);
+                final String sdpOffer = jsonMessage.get("sdpOffer").getAsString();
+                user.receiveVideoFrom(sender, sdpOffer);
+                break;
+            case "leaveRoom":
+                leaveRoom(user);
+                break;
+            case "onIceCandidate":
+                JsonObject candidate = jsonMessage.get("candidate").getAsJsonObject();
+
+                if (user != null) {
+                    IceCandidate cand = new IceCandidate(candidate.get("candidate").getAsString(),
+                            candidate.get("sdpMid").getAsString(), candidate.get("sdpMLineIndex").getAsInt());
+                    user.addCandidate(cand, jsonMessage.get("name").getAsString());
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        UserSession user = registry.removeBySession(session);
+        roomManager.getRoom(user.getRoomName()).leave(user);
+    }
+
+    private void joinRoom(JsonObject params, WebSocketSession session) throws IOException {
+        final String roomName = params.get("room").getAsString();
+        final String name = params.get("name").getAsString();
+        log.info("참가자 {}: 접속 방 이름 {}", name, roomName);
+
+        Room room = roomManager.getRoom(roomName);
+        final UserSession user = room.join(name, session);
+        registry.register(user);
+    }
+
+    private void leaveRoom(UserSession user) throws IOException {
+        final Room room = roomManager.getRoom(user.getRoomName());
+        room.leave(user);
+        if (room.getParticipants().isEmpty()) {
+            roomManager.removeRoom(room);
+        }
+    }
+}
