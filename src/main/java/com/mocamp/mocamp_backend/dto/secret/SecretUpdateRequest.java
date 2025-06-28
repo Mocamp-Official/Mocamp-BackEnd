33d6d73c0f80e6e3aca06f0c5473dfb5c615a465
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-06-28 17:34:40 +0900

    feat: 목표 시크릿 모드 웹소캣 Request DTO 생성

---
+++ src/main/java/com/mocamp/mocamp_backend/dto/secret/SecretUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.mocamp.mocamp_backend.dto.secret;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SecretUpdateRequest {
+
+    private Boolean isSecret;
+}
